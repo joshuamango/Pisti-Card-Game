37c00c15bf595f311657aca06aa3f12f56df2979
Joshua <37916234+joshuamango@users.noreply.github.com>
2018-05-08 15:37:44 -0400

    Class Cleaning

--- Player.java
+++ Player.java
@@ -1,45 +1,45 @@
 public class Player
 {
-    //Attributes
-    public Hand hand;
-    private Deck captured;
-    private boolean isTurn;
-    private double points;
+   //Attributes
+   public Hand hand;
+   private Deck captured;
+   private boolean isTurn;
+   private double points;
 
-    //Constructor
-    public Player()
-    {
-    }
+   //Constructor
+   public Player()
+   {
+   }
 
-    //Returns true if it is currently this players turn
-    public boolean isTurn()
-    {
-        return isTurn;
-    }
+   //Returns true if it is currently this players turn
+   public boolean isTurn()
+   {
+      return isTurn;
+   }
 
-    //Set turn as true or false
-    public void setTurn(boolean turn)
-    {
-        isTurn = turn;
-    }
+   //Set turn as true or false
+   public void setTurn(boolean turn)
+   {
+      isTurn = turn;
+   }
 
-    //Returns a deck with the cards that are currently in the players hand
-    public Hand getHand()
-    {
-        return hand;
-    }
+   //Returns a deck with the cards that are currently in the players hand
+   public Hand getHand()
+   {
+      return hand;
+   }
 
-    public Deck getCaptured()
-    {
-        return captured;
-    }
+   public Deck getCaptured()
+   {
+      return captured;
+   }
 
-    //Adds the points for each card captured to the player's points
-    public void getTotalPoints(Deck captured)
-    {
-        for (Card card: captured.deck)
-        {
-            this.points += card.getValue();
-        }
-    }
+   //Adds the points for each card captured to the player's points
+   public void getTotalPoints(Deck captured)
+   {
+      for (Card card: captured.deck)
+      {
+         this.points += card.getValue();
+      }
+   }
 }
